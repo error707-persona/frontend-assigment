7f8911de7507b388cbd28ced77bcf5eb0724284f
areesha <areeshasayed09@gmail.com>
2022-09-29 00:16:22 +0530

    repairs done

--- src/utils/DeleteNote.js
+++ src/utils/DeleteNote.js
@@ -3,9 +3,10 @@ import axios from "axios";
 const DeleteNote = (id) => {
         axios({
             method: 'post',
-            url: `https://secure-bastion-81443.herokuapp.com/${id}`,
+            url: `https://secure-bastion-81443.herokuapp.com/delete/${id}`,
            
           }).then(res=>{
+            console.log("ok on frontend")
             console.log("res post", res.data);
           }).catch(err=>{
             console.log("error in request", err);
